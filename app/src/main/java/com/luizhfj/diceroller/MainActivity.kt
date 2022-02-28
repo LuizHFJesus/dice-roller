101eafcc66c3219f02f923e9e2f0864ebb038592
Luiz Henrique Firmino de Jesus <luizhfj@motorola.com>
2022-02-28 16:37:52 -0300

    🎲 Adding the dice images

--- app/src/main/java/com/luizhfj/diceroller/MainActivity.kt
+++ app/src/main/java/com/luizhfj/diceroller/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.luizhfj.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -18,9 +18,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
         val randomInt = Random.nextInt(6) + 1
-        resultText.text = "$randomInt"
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
